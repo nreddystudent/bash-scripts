c19a366566ec20900f671eb2d3d75cd529b26e4f
Nolin REDDY <nreddy@c5r3s5.wethinkcode.co.za>
2019-05-22 06:25:41 +0200

    changed icon size

--- auto.sh
+++ auto.sh
@@ -12,6 +12,6 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/System Preferences.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
-defaults write com.apple.dock tilesize -int 100
+defaults write com.apple.dock tilesize -int 75
 
 killall Dock
